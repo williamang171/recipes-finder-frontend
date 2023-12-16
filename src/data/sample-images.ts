7186b1501b4a5920862087d498dcbc86e28e26a1
William Ang <williamang9174@gmail.com>
2023-12-16 15:41:24 +0800

    update default image

--- src/data/sample-images.ts
+++ src/data/sample-images.ts
@@ -36,9 +36,9 @@ export const sampleImages = [
         authorLink: 'https://unsplash.com/@miracletwentyone'
     },
     {
-        img: 'https://images.unsplash.com/photo-1663329872246-55cda1f8df62?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-        author: '@furkandokuzlar',
-        authorLink: 'https://unsplash.com/@furkandokuzlar'
+        img: 'https://images.unsplash.com/photo-1604504573541-bb5f1c53c20f?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        author: '@eugene_kuznetsov',
+        authorLink: 'https://unsplash.com/@eugene_kuznetsov'
     }
 ].map((si) => {
     return {
